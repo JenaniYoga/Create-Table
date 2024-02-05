27a9194e521c2ff8b4117d0d4e7bb89cdfbf1a63
Jenani Yoga <153412091+JenaniYoga@users.noreply.github.com>
2024-02-05 09:21:26 +0530

    Add files via upload

---
+++ Create Table.py
@@ -0,0 +1,6 @@
+from prettytable import PrettyTable
+new_table=PrettyTable(["Sno","Language","Games","Videos"])
+new_table.add_row(["01","Python","Among US","Tutorial"])
+new_table.add_row(["02","Java","FreeFire","Advanced"])
+new_table.add_row(["03","PHP","Pubg","Intermediate"])
+print(new_table)
